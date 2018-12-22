11379bb145a2b03b3cae1d0e0aa12cd4a8fa27d1
Gbenga Awodokun <gbenga.awodokun@ge.com>
2018-12-22 10:06:47 -0500

    working chatapp with Google DB

--- chatapp/server.js
+++ chatapp/server.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
 
-app.use(express.static(__dirname));
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var dbUrl = 'mongodb://newadays1:newgben1@ds145828.mlab.com:45828/gbenga-node'
+
+var Message = mongoose.model('Message',{
+    'name' : String,
+    'message' : String
+})
 var messages = [
     {name: 'Tim', message: 'Hi'},
     {name: 'Jane', message: 'Hello'}
@@ -13,6 +26,34 @@ app.get('/messages', (req, res) =>{
 
 });
 
-var server = app.listen(3000,() => {
+app.post('/messages', (req, res) =>{
+    // console.log(req.body)
+    var message = new Message(req.body)
+
+    message.save((err) => {
+
+        if (err)
+            sendStatus(500)
+
+        messages.push(req.body)
+        io.emit('message', req.body)
+        res.sendStatus(200)
+        
+        
+        
+    })
+    
+
+});
+
+io.on('connection', (socket) =>{
+    console.log("user connected")
+
+})
+
+mongoose.connect(dbUrl,{ useNewUrlParser: true }, (err) =>{
+    console.log('a user connected', err)
+})
+var server = http.listen(3000,() => {
     console.log('server is listening on port', server.address().port)
 });
