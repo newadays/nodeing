83314bfa86d6365c6de749d3b91249b3849abce7
Gbenga Awodokun <gbenga.awodokun@ge.com>
2019-01-02 22:30:23 -0500

    to fix routes

--- trafficWise/server.js
+++ trafficWise/server.js
@@ -12,12 +12,24 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.get('/traffic', (req,res) =>{
     Request.get('https://data.cityofchicago.org/resource/m65n-ux8y.json', (err, resp, body)=>{
         if(err){
-            return console.dir(error)
+            return console.dir(err)
         }
             // console.log(resp.body)
             // return resp.body
             res.send(JSON.parse(resp.body))
     })
+})
+
+app.post('/trafficstats', (req, res) =>{
+    Request.get(`https://data.cityofchicago.org/resource/m65n-ux8y.json?$where=region_id=${resp.body._region_id}`,{"headers":{"X-App-Token": "baRUbRLnaRUQk8RGJXpA2ezoV"}}, (err, resp, body)=>{
+        if(err){
+            return console.dir(err)
+        }
+
+        console.log(JSON.parse(resp.body))
+    })
+    console.log(req.body)
+    res.sendStatus(200)
 })
 
 var server = app.listen(3000, () =>{
